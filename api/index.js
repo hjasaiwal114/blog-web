71b6f5a3a6fbd9b21e78f08517202ef1adae730d
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-23 00:10:00 +0530

    installing bycrypt for securing password

--- api/index.js
+++ api/index.js
@@ -4,12 +4,16 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
   
 })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
+
+  app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
   console.log("Backend is running on port 5000.");
