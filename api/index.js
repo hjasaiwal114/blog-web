5ad277a156635132773c7db2957e67b3a6a90e14
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-22 16:47:20 +0530

    connecting mongo db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.use("/himanshu", (req, res) => {
-    console.log("hey this is main url");
-});
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
 
 app.listen(5000, () => {
-    console.log("Backend is running on port 5000.");
+  console.log("Backend is running on port 5000.");
 });
