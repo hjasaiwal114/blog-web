3aabc87882d1d3425ba4f1e2108b329b40cd1d4f
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-22 17:26:02 +0530

    creating mongoose schema

--- api/index.js
+++ api/index.js
@@ -6,8 +6,7 @@ const mongoose = require("mongoose");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  
 })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
