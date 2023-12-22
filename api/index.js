e23caeb6305c4e418b19158e0956b1578c2ef6d2
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-22 16:10:18 +0530

    creating express server

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-console.log("hello")
+app.use("/himanshu", (req, res) => {
+    console.log("hey this is main url");
+});
 
-
-app.listen("5000", () => {
-    console.log("Backend is running.")
-})
+app.listen(5000, () => {
+    console.log("Backend is running on port 5000.");
+});
