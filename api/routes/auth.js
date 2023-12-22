7859c1759d3ce24869a675a325e5bdf6d6e27169
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-22 21:38:37 +0530

    starting with authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,1 +1,18 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// Register
+router.post("/register", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedpass = await bcrypt.hash(req.body.password, salt);
+        const newUser  = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedpass,
+        });
+
+
+    }
+})
