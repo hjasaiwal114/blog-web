f2831107d88aef2c1eb7623fb283d52e723c1e2a
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-24 01:39:25 +0530

    improving code

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,25 +5,23 @@ const bcrypt = require("bcrypt");
 
 //UPDATE
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
+  try {
+    const userId = req.params.id;
+    const updatedUserId = req.body.userId;
+
+    if (userId !== updatedUserId) {
+      return res.status(401).json({error: "Unauthorized - You can update only your account!"});
+    }
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
-    res.status(401).json("You can update only your account!");
+
+    const updateUser = await User.findByIdAndUpdate(userId, { $set: req.body }, { new: true });
+    res.status(200).json(updatedUser);
+
+  } catch (error) {
+    res.status(500).json({error: "Internal Server Error" });
   }
 });
 
