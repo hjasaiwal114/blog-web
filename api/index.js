3d826cd547298c8b843f0a28d4ed070e478814a3
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-24 00:19:03 +0530

    user model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -13,7 +15,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
 
-  app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
   console.log("Backend is running on port 5000.");
