ec54b4a412259409061c3ff09941dc2090afe674
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-12-24 02:53:26 +0530

    improving documenttion

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,39 +1,60 @@
+// Import necessary modules and dependencies
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-//REGISTER
+// REGISTER route for user registration
 router.post("/register", async (req, res) => {
   try {
+    // Generate a salt for password hashing
     const salt = await bcrypt.genSalt(10);
+
+    // Hash the user's password using the generated salt
     const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new User instance with hashed password
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPass,
     });
 
+    // Save the new user to the database
     const user = await newUser.save();
+
+    // Respond with the user information
     res.status(200).json(user);
   } catch (err) {
+    // Handle errors and respond with a generic error message
     res.status(500).json(err);
   }
 });
 
-//LOGIN
+// LOGIN route for user authentication
 router.post("/login", async (req, res) => {
   try {
+    // Find a user with the provided username in the database
     const user = await User.findOne({ username: req.body.username });
+
+    // If no user is found, respond with an error message
     !user && res.status(400).json("Wrong credentials!");
 
+    // Compare the provided password with the hashed password stored in the database
     const validated = await bcrypt.compare(req.body.password, user.password);
+
+    // If the passwords do not match, respond with an error message
     !validated && res.status(400).json("Wrong credentials!");
 
+    // Exclude the password field from the user document
     const { password, ...others } = user._doc;
+
+    // Respond with the user profile (excluding the password)
     res.status(200).json(others);
   } catch (err) {
+    // Handle errors and respond with a generic error message
     res.status(500).json(err);
   }
 });
 
+// Export the router for use in other parts of the application
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,59 +1,76 @@
+// importing neccessary modules and dependencies
 const router = require("express").Router();
 const User = require("../models/User");
-const Post = require("../models/Post");
+const Post  = require("../models/Post");
 const bcrypt = require("bcrypt");
 
-//UPDATE
+// Update route for user profile
 router.put("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
-    const updatedUserId = req.body.userId;
 
-    if (userId !== updatedUserId) {
-      return res.status(401).json({error: "Unauthorized - You can update only your account!"});
+    const updateUserId = req.body.userId;
+
+    if (userId !== updateUserId) {
+        return res.status(401).json({error: "Unauthorized - You cna update only your account!" });
     }
+
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
+      req.body.password = await bcrypt.hash(req.body.password, salt)
     }
 
-    const updateUser = await User.findByIdAndUpdate(userId, { $set: req.body }, { new: true });
-    res.status(200).json(updatedUser);
+    const updateUser =  awaitUser.findByidAndUpdate(userId, { $set : req.body }, {new: true});
 
+    res.status(200).json(updateUser);
   } catch (error) {
-    res.status(500).json({error: "Internal Server Error" });
+    res.status(500).json({error: "Internal Sevrer Error"});
   }
 });
 
-//DELETE
+// Delete
 router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted...");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } catch (err) {
-      res.status(404).json("User not found!");
+  try {
+    const userId = req.params.id;
+    const requestingUserId = req.body.userId;
+
+    if (userId !== requestingUserId) {
+      return res.status(401).json({error: "Unauthorized - You can delete only your account! "});
     }
-  } else {
-    res.status(401).json("You can delete only your account!");
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    await Post.deleteMany({ username: user.name });
+
+    await User.findByIdAndDelete(userId);
+
+    res.status(500).json({error: "Internal Server Error" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server Error"});
   }
 });
 
-//GET USER
+// Get route for retrieving user profile by ID
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
+    const userId = req.params.id;
+
+    const user  = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found"})
+    }
+
+    const { password, ...others } = user._docs;
     res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Sevrer Error"});
   }
 });
 
+// export router for use in other parts of teh applicaton
 module.exports = router;
